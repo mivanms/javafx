7b845d20e15f95f2e146ae21899ada5bd356d47b
mmourelo <mmourelo@LES000804.corp.capgemini.com>
2018-05-30 18:28:20 +0200

    Tercera subida: Nuevas Clases

---
+++ jfxtest/src/main/java/com/inditex/jfxtext/model/persistence/dao/FactoryDAO.java
@@ -0,0 +1,41 @@
+package com.inditex.jfxtext.model.persistence.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FactoryDAO {
+	private Connection connection;
+	private static final String DRIVER = "org.h2.Driver";
+	private static final String BD = "jdbc:h2:~/test";
+	private static final String USUARIO = "admin";  
+	private static final String PASSWORD = "admin";  
+	
+	public FactoryDAO() { }
+	
+	protected Connection conectar() {  
+		try {
+	
+			Class.forName(DRIVER);
+			connection = DriverManager.getConnection(BD, USUARIO, PASSWORD);
+		
+	} catch (ClassNotFoundException e) { 
+		e.printStackTrace(); 
+	} catch (SQLException e) { 
+		e.printStackTrace();
+		}
+		
+		return connection;
+	}
+	
+	
+	protected void desconectar() {
+	try {
+	connection.close();
+	} catch (SQLException e) {e.printStackTrace();}
+	}
+	
+	
+	public Connection getConnection() { return connection; }
+
+}
